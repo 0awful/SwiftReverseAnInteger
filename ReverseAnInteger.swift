7ce0c6dc54a797501a2a2d4943798904d99f5d2b
imizaac <im.izaac.seo@gmail.com>
2017-11-05 21:57:50 -0800

    Added the solution

---
+++ ReverseAnInteger.swift
@@ -0,0 +1,38 @@
+//Given a 32-bit signed integer, reverse digits of an integer.
+//
+//Example 1:
+//
+//Input: 123
+//Output:  321
+//Example 2:
+//
+//Input: -123
+//Output: -321
+//Example 3:
+//
+//Input: 120
+//Output: 21
+//Note:
+//Assume we are dealing with an environment which could only hold integers within the 32-bit signed integer range. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+
+class Solution {
+    func reverse(_ x: Int) -> Int {
+        var temp = x
+        var result = 0
+        
+        while temp != 0 {
+            let last = temp % 10
+            let newResult = result * 10 + last
+            if newResult > 2147483647 || newResult < -2147483647 {
+                return 0
+            }
+            result = newResult
+            temp = temp / 10
+        }
+        
+        return result
+    }
+}
+
+
